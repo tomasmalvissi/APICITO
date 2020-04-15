1c32512141c6a772c4bcd0a4043ded2a4222f779
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-04-15 17:56:06 -0300

    add solution

---
+++ APICITO/Controllers/ClientesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICITO.Modelos;
+using APICITO.Repositorios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICITO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPClientes rpCli = new RPClientes();
+            return Ok(rpCli.ObtenerClientes());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPClientes rpCli = new RPClientes();
+
+            var cliRet = rpCli.ObtenerCliente(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("El cliente " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarCliente(Cliente nuevoCliente)
+        {
+            RPClientes rpCli = new RPClientes();
+            rpCli.Agregar(nuevoCliente);
+            return CreatedAtAction(nameof(AgregarCliente), nuevoCliente);
+        }
+    }
+}
---
+++ APICITO/Repositorios/RPClientes.cs
@@ -0,0 +1,35 @@
+﻿using APICITO.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICITO.Repositorios
+{
+    public class RPClientes
+    {
+        public static List<Cliente> _listaClientes = new List<Cliente>()
+        {
+            new Cliente() { Id = 1, Nombre = "Cliente 1" , Apellido = "Apellido 1" },
+            new Cliente() { Id = 2, Nombre = "Cliente 2" , Apellido = "Apellido 2" },
+            new Cliente() { Id = 3, Nombre = "Cliente 3" , Apellido = "Apellido 3" }
+        };
+
+        public IEnumerable<Cliente> ObtenerClientes()
+        {
+            return _listaClientes;
+        }
+
+        public Cliente ObtenerCliente(int id)
+        {
+            var cliente = _listaClientes.Where(cli => cli.Id == id);
+
+            return cliente.FirstOrDefault();
+        }
+
+        public void Agregar(Cliente nuevoCliente)
+        {
+            _listaClientes.Add(nuevoCliente);
+        }
+    }
+}
